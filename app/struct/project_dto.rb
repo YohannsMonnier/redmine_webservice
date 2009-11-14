60f723ac5b2cefedfd3d14ea8fe640b2bfc56c62
Yohann Monnier <yohann.monnier@gmail.com>
2009-11-14 15:28:36 +0100

    modified:   app/struct/project_dto.rb 	modified:   init.rb

--- app/struct/project_dto.rb
+++ app/struct/project_dto.rb
@@ -1,19 +1,9 @@
 class ProjectDto < ActionWebService::Struct
-#          <id xsi:type="xsd:int">1</id>
-#          <name xsi:type="xsd:string">Redmine Mylyn Webservice</name>
-#          <description xsi:type="xsd:string"></description>
-#          <homepage xsi:type="xsd:string"></homepage>
-#          <is_public xsi:type="xsd:boolean">true</is_public>
-#          <parent_id xsi:nil="true"></parent_id>
-#          <projects_count xsi:type="xsd:int">0</projects_count>
-#          <created_on xsi:type="xsd:dateTime">2008-05-13T19:40:41+02:00</created_on>
-#          <updated_on xsi:type="xsd:dateTime">2008-05-24T15:02:28+02:00</updated_on>
-#          <identifier xsi:type="xsd:string">redmine-mylyn</identifier>
-#          <status xsi:type="xsd:int">1</status>
 
   member :id, :int
   member :identifier, :string
   member :name, :string
+  member :short_description, :string
   member :issue_edit_allowed, :boolean
   member :project_saved, :boolean
   member :new_project, :boolean
@@ -23,8 +13,8 @@ class ProjectDto < ActionWebService::Struct
       :id => project.id,
       :identifier => project.identifier,
       :name => project.name,
+      :short_description => project.short_description,
       :issue_edit_allowed => User.current.allowed_to?(:edit_issues, project)
- #     :issueCustomFiels => project.custom_fields
     )
   end
   
@@ -33,6 +23,7 @@ class ProjectDto < ActionWebService::Struct
       :id => project.id,
       :identifier => project.identifier,
       :name => project.name,
+      :short_description => project.short_description,
       :issue_edit_allowed => User.current.allowed_to?(:edit_issues, project),
       :project_saved => boo_saved,
       :new_project => boo_new
